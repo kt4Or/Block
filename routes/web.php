a70e38c1039011ec2d77dc70839cd834044ac815
kiri <yara1@gmail.com>
2026-05-13 11:06:21 +0500

    PR-11

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\MainController;
 use App\Http\Controllers\Admin\PostController;
 use App\Http\Controllers\Admin\TagController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -28,3 +29,9 @@ Route::group(['prefix' => 'admin'], function (){
      Route::resource('/posts', PostController::class);
 });
 
+Route::get('/register', [UserController::class, 'create'])->name('register.create');
+Route::post('/register', [UserController::class, 'store'])->name('register.store');
+
+Route::get('/', function () {
+    return view('welcome');
+})->name('home');
