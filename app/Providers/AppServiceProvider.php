2cffe84b7fc59f29fb95113626574c2b134e4288
kiri <yara1@gmail.com>
2026-05-19 14:46:50 +0500

    PR-17

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Category;
+use App\Models\Post;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        view()->composer('layouts.sidebar', function ($view){
+            $view->with('popular_posts', Post::orderBy('views', 'desc')->limit(3)->get());
+
+            $view->with('cats', Category::withCount('posts')->orderBy('posts_count', 'desc')->get());
+        });
+
+
     }
 }
